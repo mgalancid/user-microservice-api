35c107df20e5d459e7d320bfc11516abdb1b1309
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-21 16:08:14 -0300

    Implemented UserEntityController CRUD

--- user-service/src/main/java/com/mindhub/user_service/controllers/UserEntityController.java
+++ user-service/src/main/java/com/mindhub/user_service/controllers/UserEntityController.java
@@ -1,4 +1,43 @@
 package com.mindhub.user_service.controllers;
 
+import com.mindhub.user_service.dtos.NewUserEntityDTO;
+import com.mindhub.user_service.dtos.UserEntityDTO;
+import com.mindhub.user_service.models.RoleType;
+import com.mindhub.user_service.services.impl.UserEntityServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserEntityController {
+
+    @Autowired
+    private UserEntityServiceImpl userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserEntityDTO>> getAllUsers() {
+        List<UserEntityDTO> users = userService.getAllUsersDTO();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<NewUserEntityDTO> createNewUser(@RequestBody NewUserEntityDTO newUserDTO) {
+        NewUserEntityDTO createdUser = userService.createNewUser(newUserDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<RoleType>> getAllRoles() {
+        List<RoleType> roles = userService.getAllRoles();
+        return ResponseEntity.ok(roles);
+    }
 }
+
