f49faf0242c8de129e94d749f674ef13f69351b0
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-21 15:54:50 -0300

    Added UserService Implementation

--- user-service/src/main/java/com/mindhub/user_service/services/impl/UserEntityServiceImpl.java
+++ user-service/src/main/java/com/mindhub/user_service/services/impl/UserEntityServiceImpl.java
@@ -2,11 +2,14 @@ package com.mindhub.user_service.services.impl;
 
 import com.mindhub.user_service.dtos.NewUserEntityDTO;
 import com.mindhub.user_service.dtos.UserEntityDTO;
+import com.mindhub.user_service.models.RoleType;
+import com.mindhub.user_service.models.UserEntity;
 import com.mindhub.user_service.repositories.UserEntityRepository;
 import com.mindhub.user_service.services.UserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserEntityServiceImpl implements UserEntityService {
 
@@ -15,16 +18,31 @@ public class UserEntityServiceImpl implements UserEntityService {
 
     @Override
     public List<UserEntityDTO> getAllUsersDTO() {
-        return List.of();
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserEntityDTO(user.getId(),
+                                                        user.getUsername(),
+                                                        user.getEmail()))
+                .collect(Collectors.toList()
+                );
     }
 
     @Override
     public NewUserEntityDTO createNewUser(NewUserEntityDTO newUserDTO) {
-        return null;
+        UserEntity user = new UserEntity();
+        user.setUsername(newUserDTO.getUsername());
+        user.setEmail(newUserDTO.getEmail());
+        user.setRole(newUserDTO.getRole());
+        UserEntity savedUser = userRepository.save(user);
+
+        return new NewUserEntityDTO(savedUser.getUsername(),
+                                    savedUser.getEmail(),
+                                    savedUser.getRole()
+        );
     }
 
     @Override
-    public List<UserEntityDTO> getAllRoles() {
-        return List.of();
+    public List<RoleType> getAllRoles() {
+        return List.of(RoleType.values());
     }
 }
