354f6d64c71af1ea9227dc033f6923764ad220bd
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-21 15:43:39 -0300

    Added properties & constructor

--- user-service/src/main/java/com/mindhub/user_service/dtos/UserEntityDTO.java
+++ user-service/src/main/java/com/mindhub/user_service/dtos/UserEntityDTO.java
@@ -1,4 +1,25 @@
 package com.mindhub.user_service.dtos;
 
 public class UserEntityDTO {
+    private final Long id;
+    private final String username;
+    private final String email;
+
+    public UserEntityDTO(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
