3ccfac571858d4e3dc656a3f9e8cc132159a4a05
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-21 15:28:23 -0300

    Added Service Methods

--- user-service/src/main/java/com/mindhub/user_service/services/UserEntityService.java
+++ user-service/src/main/java/com/mindhub/user_service/services/UserEntityService.java
@@ -1,4 +1,12 @@
 package com.mindhub.user_service.services;
 
+import com.mindhub.user_service.dtos.NewUserEntityDTO;
+import com.mindhub.user_service.dtos.UserEntityDTO;
+
+import java.util.List;
+
 public interface UserEntityService {
+    List<UserEntityDTO> getAllUsersDTO();
+    NewUserEntityDTO createNewUser(NewUserEntityDTO newUserDTO);
+    List<UserEntityDTO> getAllRoles();
 }
