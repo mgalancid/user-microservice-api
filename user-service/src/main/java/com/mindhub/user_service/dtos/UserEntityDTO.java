35fe8a7abe88e0e431e08b98340156bf4c0a3631
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-22 10:09:46 -0300

    Refactored UserEntityDTO & NewUserEntityDTO

--- user-service/src/main/java/com/mindhub/user_service/dtos/UserEntityDTO.java
+++ user-service/src/main/java/com/mindhub/user_service/dtos/UserEntityDTO.java
@@ -1,14 +1,16 @@
 package com.mindhub.user_service.dtos;
 
+import com.mindhub.user_service.models.UserEntity;
+
 public class UserEntityDTO {
     private final Long id;
     private final String username;
     private final String email;
 
-    public UserEntityDTO(Long id, String username, String email) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
+    public UserEntityDTO(UserEntity user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
     }
 
     public Long getId() {
