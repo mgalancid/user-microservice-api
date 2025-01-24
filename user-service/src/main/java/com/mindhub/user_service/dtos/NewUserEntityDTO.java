4aada3aaa4cbe85d793907f95680208b0702659d
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-24 15:47:44 -0300

    Refactored User

--- user-service/src/main/java/com/mindhub/user_service/dtos/NewUserEntityDTO.java
+++ user-service/src/main/java/com/mindhub/user_service/dtos/NewUserEntityDTO.java
@@ -1,18 +1,16 @@
 package com.mindhub.user_service.dtos;
 
 import com.mindhub.user_service.models.RoleType;
-import com.mindhub.user_service.models.UserEntity;
-import org.apache.catalina.User;
 
 public class NewUserEntityDTO {
     private final String username;
     private final String email;
     private final RoleType role;
 
-    public NewUserEntityDTO(UserEntity user) {
-        this.username = user.getUsername();
-        this.email = user.getEmail();
-        this.role = user.getRole();
+    public NewUserEntityDTO(String username, String email, RoleType role) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
     }
 
     public String getUsername() {
